00e815880a00fa57aa008d3c7741ef1e855c7343
as-github-repo <maymyatthwe.awitd@gmail.com>
2018-08-29 10:20:40 +0630

    activity-no-ssh-test

--- app/src/main/java/com/example/user/nossh2/MainActivity.java
+++ app/src/main/java/com/example/user/nossh2/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ssh);
+        setContentView(R.layout.activity_no_ssh_test);
     }
 }
